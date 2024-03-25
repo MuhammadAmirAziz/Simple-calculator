c2caf922fca79f3b6cc800159a6fa14669b89264
unknown <you@example.com>
2024-03-26 03:22:44 +0500

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const asnwer = await inquirer.prompt([
+  {
+    message: "Enter First Number",
+    type: "number",
+    name: "firstnumber",
+  },
+  { message: "Enter Second Number", type: "number", name: "secondnumber" },
+  {
+    message: "Selet one of the operators to perform actoin",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "subtraction", "Multiplication", "Division"],
+  },
+]);
+
+if (asnwer.operator === "Addition"){
+    console.log(asnwer.firstnumber + asnwer.secondnumber);
+} else if(asnwer.operator === "subtraction"){
+    console.log(asnwer.firstnumber - asnwer.secondnumber);
+} else if(asnwer.operator === "Multiplication"){
+    console.log(asnwer.firstnumber * asnwer.secondnumber);
+} else if(asnwer.operator === "Division"){
+    console.log(asnwer.firstnumber / asnwer.secondnumber);
+} else{
+    console.log("Please select valid number")
+};
+
